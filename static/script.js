14309dbb998ffa7e46f1dd9a7052311429cd151d
metjush <metjush@gmail.com>
2016-03-29 09:27:50 +0200

    make script for disabling item selects

---
+++ static/script.js
@@ -0,0 +1,16 @@
+function showItems() {
+    categorySelect = document.getElementById('category');
+    selectedCategory = categorySelect.value;
+    itemId = 'item-' + selectedCategory;
+    // itemSelect = document.getElementById(itemId);
+    allItemSelects = document.getElementsByClassName('item');
+    for (var i = 0; i < allItemSelects.length; i++) {
+        if (allItemSelects[i].id == itemId) {
+            // keep this open
+            allItemSelects[i].disabled = false;
+        } else {
+            // hide it
+            allItemSelects[i].disabled = true;
+        }
+    }
+}
