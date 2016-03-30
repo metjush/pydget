82e4f157c131099589d31f407c6525f0b6b73261
metjush <metjush@gmail.com>
2016-03-30 09:51:32 +0200

    add android/material date picker

--- static/script.js
+++ static/script.js
@@ -17,4 +17,9 @@ function showItems() {
 
 $(document).ready(function(){
     $(".button-collapse").sideNav();
+
+    $('.datepicker').pickadate({
+       selectMonths: true, // Creates a dropdown to control month
+       selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 });
